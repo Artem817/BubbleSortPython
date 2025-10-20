a305e787251b5d78585dc3b6f8e68b289a628c72
Artem817 <pavartem769@gmail.com>
2025-10-20 14:16:46 +0300

    optimize bubble sort by tracking last swap position

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,18 +1,14 @@
 def bubble_sort(data_list):
     n = len(data_list)
-    
-    for i in range(n):
-        swapped = False 
-        
-        for j in range(0, n - i - 1):
-            if data_list[j] > data_list[j + 1]:
-                data_list[j], data_list[j + 1] = data_list[j + 1], data_list[j]
-                swapped = True
-        
-        if not swapped:
-            break
-            
+    while n > 0:
+        new_n = 0
+        for i in range(1, n):
+            if data_list[i - 1] > data_list[i]:
+                data_list[i - 1], data_list[i] = data_list[i], data_list[i - 1]
+                new_n = i  
+        n = new_n  
     return data_list
+
 
 my_list = [64, 34, 25, 12, 22, 11, 90]
 
